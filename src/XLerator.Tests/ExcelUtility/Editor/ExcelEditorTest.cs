c232982268f3514e172d1d21081a170a64117efe
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-05-17 11:32:12 +0200

    Update ExcelEditorTest.cs

--- src/XLerator.Tests/ExcelUtility/Editor/ExcelEditorTest.cs
+++ src/XLerator.Tests/ExcelUtility/Editor/ExcelEditorTest.cs
@@ -40,30 +40,28 @@ public class ExcelEditorTest
         testee.Dispose();
         
         // Assert
-        using (var spreadsheetDocument = SpreadsheetDocument.Open(filePath, false))
-        {
-            var workbookPart = spreadsheetDocument.WorkbookPart;
-            var worksheetPart = workbookPart?.WorksheetParts.First();
-            var sheetData = worksheetPart?.Worksheet.Elements<SheetData>().First();
-            var rows = sheetData?.Elements<Row>().ToList();
+        using var spreadsheetDocument = SpreadsheetDocument.Open(filePath, false);
+        var workbookPart = spreadsheetDocument.WorkbookPart;
+        var worksheetPart = workbookPart?.WorksheetParts.First();
+        var sheetData = worksheetPart?.Worksheet.Elements<SheetData>().First();
+        var rows = sheetData?.Elements<Row>().ToList();
             
-            // Assert
-            rows.Should().NotBeNull();
-            rows!.Count.Should().Be(1);
+        // Assert
+        rows.Should().NotBeNull();
+        rows!.Count.Should().Be(1);
             
-            var newRow = rows.First();
-            var cells = newRow.Elements<Cell>().ToList();
+        var newRow = rows.First();
+        var cells = newRow.Elements<Cell>().ToList();
             
-            // Assert
-            cells.Count.Should().Be(2);
+        // Assert
+        cells.Count.Should().Be(2);
             
-            var firstHeaderValue = cells[0].InnerText;
-            var secondHeaderValue = cells[1].InnerText;
+        var firstHeaderValue = cells[0].InnerText;
+        var secondHeaderValue = cells[1].InnerText;
             
-            // Assert
-            firstHeaderValue.Should().Be(data.Id.ToString());
-            secondHeaderValue.Should().Be(data.Name);
-        }
+        // Assert
+        firstHeaderValue.Should().Be(data.Id.ToString());
+        secondHeaderValue.Should().Be(data.Name);
     }
     
     [Test]
@@ -102,42 +100,40 @@ public class ExcelEditorTest
         testee.Dispose();
         
         // Assert
-        using (var spreadsheetDocument = SpreadsheetDocument.Open(filePath, false))
-        {
-            var workbookPart = spreadsheetDocument.WorkbookPart;
-            var worksheetPart = workbookPart?.WorksheetParts.First();
-            var sheetData = worksheetPart?.Worksheet.Elements<SheetData>().First();
-            var rows = sheetData?.Elements<Row>().ToList();
+        using var spreadsheetDocument = SpreadsheetDocument.Open(filePath, false);
+        var workbookPart = spreadsheetDocument.WorkbookPart;
+        var worksheetPart = workbookPart?.WorksheetParts.First();
+        var sheetData = worksheetPart?.Worksheet.Elements<SheetData>().First();
+        var rows = sheetData?.Elements<Row>().ToList();
             
-            // Assert
-            rows.Should().NotBeNull();
-            rows!.Count.Should().Be(2);
+        // Assert
+        rows.Should().NotBeNull();
+        rows!.Count.Should().Be(2);
             
-            var firstRow = rows.First();
-            var cells = firstRow.Elements<Cell>().ToList();
+        var firstRow = rows.First();
+        var cells = firstRow.Elements<Cell>().ToList();
             
-            // Assert
-            cells.Count.Should().Be(2);
+        // Assert
+        cells.Count.Should().Be(2);
             
-            var firstHeaderValue = cells[0].InnerText;
-            var secondHeaderValue = cells[1].InnerText;
+        var firstHeaderValue = cells[0].InnerText;
+        var secondHeaderValue = cells[1].InnerText;
             
-            // Assert
-            firstHeaderValue.Should().Be(data.Id.ToString());
-            secondHeaderValue.Should().Be(data.Name);
+        // Assert
+        firstHeaderValue.Should().Be(data.Id.ToString());
+        secondHeaderValue.Should().Be(data.Name);
             
-            var secondRow = rows[1];
-            cells = secondRow.Elements<Cell>().ToList();
+        var secondRow = rows[1];
+        cells = secondRow.Elements<Cell>().ToList();
             
-            // Assert
-            cells.Count.Should().Be(2);
+        // Assert
+        cells.Count.Should().Be(2);
             
-            firstHeaderValue = cells[0].InnerText;
-            secondHeaderValue = cells[1].InnerText;
+        firstHeaderValue = cells[0].InnerText;
+        secondHeaderValue = cells[1].InnerText;
             
-            // Assert
-            firstHeaderValue.Should().Be(data2.Id.ToString());
-            secondHeaderValue.Should().Be(data2.Name);
-        }
+        // Assert
+        firstHeaderValue.Should().Be(data2.Id.ToString());
+        secondHeaderValue.Should().Be(data2.Name);
     }
 }
